6fa210f2391b5a1ecde69570a02cb7b878974023
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-07-04 20:24:53 +0100

    Just One Bug Left To Fix

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+	name: "LNPopupController",
+	platforms: [
+		.iOS(.v12),
+		.macOS(.v10_15)
+	],
+	products: [
+		.library(
+			name: "LNPopupController",
+			type: .dynamic,
+			targets: ["LNPopupController"]),
+		.library(
+			name: "LNPopupController-Static",
+			type: .static,
+			targets: ["LNPopupController"]),
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "LNPopupController",
+			dependencies: [],
+			path: "LNPopupController",
+			publicHeadersPath: "include",
+			cSettings: [
+				.headerSearchPath("."),
+				.headerSearchPath("Private"),
+			]),
+	]
+)
